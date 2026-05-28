e3c490a58af88ff14f7d2f074b0ae6a7b471d8e3
HoshinoTented <hoshinotented@qq.com>
2026-05-28 14:44:18 +0800

    nix: substituter priority

--- packages.nix
+++ packages.nix
@@ -5,8 +5,8 @@
   # Some vscode extensions are unfree, so...
   nixpkgs.config.allowUnfree = true;
   # .substituers is only available for root, .trusted-substituters is for non-root users
-  nix.settings.trusted-substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
-  nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+  nix.settings.trusted-substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=5" ];
+  nix.settings.substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=5" ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   programs.firefox.enable = true;
