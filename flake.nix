77e71d20c9d6ebdfc7684b9ae659f516be64d134
HoshinoTented <hoshinotented@qq.com>
2026-06-19 09:22:20 +0800

    pkg: prismterminal

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     prismterminal = {
-      url = "path:/home/hoshino/.config/nixos/prismterminal";
+      url = "github:HoshinoTented/nix-prismterminal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, prismterminal, ... }: {
     nixosConfigurations."hoshino-nix" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit home-manager; };
       modules = [
